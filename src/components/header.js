921d915785b4bd4f86297daca57bf06a4ff85eb2
Aaron Taylor <me@aarontaylor.pro>
2020-02-06 11:30:58 -0500

    SCSS implementation

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,47 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "../css/header.module.scss"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
+      class="container"
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+
+    <ul>
+      <li class="navItem link"><Link to="/page-2/">Home</Link></li>
+      <li class="navItem link"><Link to="/page-2/">Vision</Link></li>
+      <li class="navItem link"><Link to="/page-2/">Goals</Link></li>
+      <li class="navItem logo">
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `#555`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </li>
+      <li  class="navItem link"><Link to="/page-2/">Work</Link></li>
+      <li  class="navItem link"><Link to="/page-2/">Pricing</Link></li>
+      <li  class="navItem link"><Link to="/page-2/">Contact</Link></li>
+    </ul>
+  
+  
+     
     </div>
   </header>
 )
